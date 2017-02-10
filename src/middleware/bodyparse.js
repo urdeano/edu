1091d6dd6b7ac768201e4b289b4e763f2fc0b05e
urdeano <zhaoxee@gmail.com>
2017-02-10 20:58:20 +0800

    修改index为nunjucks模板

--- src/middleware/bodyparse.js
+++ src/middleware/bodyparse.js
@@ -2,7 +2,6 @@ import querystring from 'querystring';
 
 export default (req, res, next) => {
     if ( req.method.toLowerCase()==="get" ) {
-        console.log('进入 body-parser 了')
         return next()
     }
     let data = ''
